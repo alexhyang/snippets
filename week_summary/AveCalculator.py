1a9b37bc70d3b784326e3c52591ddb45dfaab82f
Alex Yang <alex.hyang.dev@gmail.com>
2023-08-20 19:46:38 -0700

    update week summary snippets

--- week_summary/AveCalculator.py
+++ week_summary/AveCalculator.py
@@ -1,14 +1,14 @@
 import statistics
+import re
 
 class AveCalculator:
-  def __init__(self, n, data):
-    self.n = n
-    self.data = data
+  def __init__(self, data):
+    self.data = re.split(r',\s*', data)
 
   def print_average(self):
     # if (data contains time in format of HH:MM), convert time to minutes, 
     #   get average in minutes, return average in format of HH:MM
-    if ':' in self.data[0]:
+    if self.isTime(self.data[0]):
 
       times = [self.convert_to_mins(time) for time in self.data]
       average_in_mins = int(statistics.fmean(times))
@@ -19,11 +19,13 @@ class AveCalculator:
     else:
       average = int(statistics.fmean([int(value) for value in self.data]))
       print(f"Average value: {average}")
+  
+  def isTime(self, str):
+    return ':' in str
 
   def convert_to_mins(self, str):
-    hours = int(str.split(":")[0])
-    minutes = int(str.split(":")[1])
-    return hours * 60 + minutes
+    [hours, minutes] = str.split(":")
+    return int(hours) * 60 + int(minutes)
     
   def convert_to_HHMM(self, num):
     hours = num // 60
--- week_summary/main.py
+++ week_summary/main.py
@@ -1,19 +1,11 @@
 from AveCalculator import AveCalculator
 
-def take_raw_values(n):
-  arr = []
-  for i in range(0, n):
-    value = input('Enter value: ')
-    arr.append(value)
-  return arr
-
 def main():
   q = "a"
   while q != 'q':
     try:
-      n = int(input('How many values do you have: '))
-      arr = take_raw_values(n)
-      calculator = AveCalculator(n, arr)
+      data = input('Enter your data: ')
+      calculator = AveCalculator(data)
       calculator.print_average()
       q = input('press q to exit, any other key to continue: ')
       print()
